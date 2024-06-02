5d639ab2835b39fb314539b8b702818d170f6498
Lucca Miorelli <lucca.miorelli@gmail.com>
2024-06-02 12:41:29 -0300

    feat: download all leverkusen games 23-24

--- football_analysis/statsbomb_data.py
+++ football_analysis/statsbomb_data.py
@@ -2,17 +2,37 @@ import requests
 import json
 import pandas as pd
 
-TEST_URL =  "https://raw.githubusercontent.com/statsbomb/open-data/533862946a73608c134d18b78226b6371ce7173c/data/events/3895052.json"
-
-resp = requests.get(TEST_URL)
-data = json.loads(resp.text)
-# print(data)
-
-# Save as json file in data folder
-# with open('data/3895052.json', 'w') as f:
-#     json.dump(data, f)
+from football_analysis.config.constant import (
+    BAYER_LEVERKUSEN_GAMES_BUNDESLIGA_23_24,
+    DATA_REPOSITORY_URL
+)
 
 
-# Explore the data
-print(data.keys())
+
+def main(
+    game_ids:list=BAYER_LEVERKUSEN_GAMES_BUNDESLIGA_23_24,
+    data_repository_url:str=DATA_REPOSITORY_URL,
+    local_data_folder:str='data/events/'
+
+):
+    for game_id in game_ids:
+        print(f"Downloading {game_id=}")
+        resp = requests.get(data_repository_url.format(game_id))
+        data = json.loads(resp.text)
+
+        # Save as json file in data folder
+        file_path = f'{local_data_folder}{game_id}.json'
+
+        with open(file_path, 'w') as f:
+            json.dump(data, f)
+        
+        print(f"Saved {game_id=}")
+
+
+if __name__ == "__main__":
+    main(
+        game_ids=BAYER_LEVERKUSEN_GAMES_BUNDESLIGA_23_24,
+        data_repository_url=DATA_REPOSITORY_URL,
+        local_data_folder='data/events/'
+    )
 

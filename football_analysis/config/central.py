07187cdd3e15234b3b74b843a58aa31ef52a39b8
Lucca Miorelli <lucca.miorelli@gmail.com>
2024-06-16 20:02:30 -0300

    feat: add minio bucket

--- football_analysis/config/central.py
+++ football_analysis/config/central.py
@@ -1,4 +1,22 @@
-formation_dict = {1: 'GK', 2: 'RB', 3: 'RCB', 4: 'CB', 5: 'LCB', 6: 'LB', 7: 'RWB',
-                  8: 'LWB', 9: 'RDM', 10: 'CDM', 11: 'LDM', 12: 'RM', 13: 'RCM',
-                  14: 'CM', 15: 'LCM', 16: 'LM', 17: 'RW', 18: 'RAM', 19: 'CAM',
-                  20: 'LAM', 21: 'LW', 22: 'RCF', 23: 'ST', 24: 'LCF', 25: 'SS'}
+from typing import Optional
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class CentralConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+    # MinIO
+    MINIO_ROOT_USER: Optional[str] = "admin"
+    MINIO_ROOT_PASSWORD: Optional[str] = "Admin@123"
+    MINIO_DEFAULT_BUCKET: Optional[str] = "football-analysis"
+
+    # AWS
+    AWS_BUCKET_NAME: str = "football-data-platform"
+    AWS_ACCESS_KEY_ID: str = "admin"
+    AWS_SECRET_ACCESS_KEY: str = "Admin@123"
+    AWS_DEFAULT_REGION: str = "us-east-1"
+    AWS_ENDPOINT_URL: Optional[str] = "http://localhost:9000"
+
+
+cc: CentralConfig = CentralConfig()
